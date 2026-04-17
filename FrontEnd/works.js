a86432dbda9cde27524a6c761b2b0e8c14c56d73
aurelienlozes <aurelienlozes@gmail.com>
2026-04-17 16:59:09 +0200

    Affichage des travaux dans la section "portfolio"

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,1 +1,17 @@
-console.log("Hello, World! This is a JavaScript file named works.js.");
+// Récupération des travaux depuis l'API
+const requete = await fetch("http://localhost:5678/api/works");
+const works = await requete.json();
+
+// Affichage des travaux dans la section "portfolio"
+const gallery = document.querySelector(".gallery");
+works.forEach(work => {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    img.src = work.imageUrl;
+    img.alt = work.title;
+    const figcaption = document.createElement("figcaption");    
+    figcaption.textContent = work.title;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+});
