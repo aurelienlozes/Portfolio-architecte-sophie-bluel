0c90e78325cd6f52b113d61e5f5c6998aa27dcfd
aurelienlozes <aurelienlozes@gmail.com>
2026-04-21 10:25:39 +0200

    Filters : add filter function

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -5,17 +5,21 @@ console.log(works);
 
 // Affichage des travaux dans la section "portfolio"
 const gallery = document.querySelector(".gallery");
-works.forEach(work => {
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    img.src = work.imageUrl;
-    img.alt = work.title;
-    const figcaption = document.createElement("figcaption");    
-    figcaption.textContent = work.title;
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    gallery.appendChild(figure);
-});
+
+function displayWorks(works) {
+    gallery.innerHTML = "";
+    works.forEach(work => {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        const figcaption = document.createElement("figcaption");
+        figcaption.textContent = work.title;
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    });
+}
 
 
 // Récupération des catégories depuis l'API
@@ -26,8 +30,19 @@ console.log(categories);
 // Création des catégories dans la section "filtres"
 const filtersContainer = document.querySelector(".filters");
 categories.forEach(category => {
+    // ajout de bouttons de filtre pour chaque catégorie
     const a = document.createElement("a");
     a.textContent = category.name;
     a.setAttribute("href", `#`);
+    a.setAttribute("data-filter", category.id);
     filtersContainer.appendChild(a);
+
+    // ajout d'un écouteur d'événement pour filtrer les travaux lors du clic sur une catégorie
+    a.addEventListener("click", (event) => {
+        //console.log(`Filtre sélectionné : ${category.name}`);
+        event.preventDefault(); // Empêche le comportement par défaut du lien
+        const filter = event.target.getAttribute("data-filter");
+        const filteredWorks = filter === "all" ? works : works.filter(work => work.categoryId == filter);
+        displayWorks(filteredWorks);        
+    });
 });
