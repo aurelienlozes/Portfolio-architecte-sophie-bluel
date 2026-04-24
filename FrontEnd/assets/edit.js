2d79ac0985d250bffb9bfeb42255d89d14abf029
aurelienlozes <aurelienlozes@gmail.com>
2026-04-24 13:15:38 +0200

    Modal : gestion de l'ouverture et fermeture de la modale

--- FrontEnd/assets/edit.js
+++ FrontEnd/assets/edit.js
@@ -7,9 +7,38 @@ if (!token || token === "") {
     displayEditMode(true);
 }
 
+/** Affiche ou masque les éléments de la page en fonction du mode d'édition
+ * @param {boolean} value - true pour afficher les éléments d'édition, false pour les masquer
+ */
 function displayEditMode(value) {
     const editModeElements = document.querySelectorAll(".edit-mode");
     editModeElements.forEach(element => {
         element.style.display = value ? "block" : "none";
     });
+}
+
+/* Affiche la popup modale d'édition lorsque le bouton "Modifier" est cliqué */
+document.querySelector(".open-modal").addEventListener("click", () => {
+    displayModal(true);
+});
+
+/* Ferme la popup modale d'édition lorsque le bouton de fermeture est cliqué */
+document.querySelector(".close-modal").addEventListener("click", () => {
+    displayModal(false);
+});
+
+/* Ferme la popup modale d'édition lorsque l'utilisateur clique en dehors de la modale */
+document.querySelector(".modal-overlay").addEventListener("click", () => {
+    if (event.target === document.querySelector(".modal-overlay")) {
+        displayModal(false);
+    }
+});
+
+/* Gère l'affichage de la popup modale d'édition
+    * @param {boolean} value - true pour afficher la modale, false pour la masquer  
+*/
+function displayModal(value) {
+    const modal = document.querySelector(".modal-overlay");
+    modal.style.display = value ? "block" : "none";
+    modal.setAttribute("aria-hidden", !value);
 }
