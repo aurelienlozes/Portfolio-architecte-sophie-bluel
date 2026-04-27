b86a87501a140b97214b1c2b4874d1a1d7b40c7b
aurelienlozes <aurelienlozes@gmail.com>
2026-04-27 10:37:20 +0200

    Edit : gestion du logout

--- FrontEnd/assets/edit.js
+++ FrontEnd/assets/edit.js
@@ -2,9 +2,11 @@ const token = localStorage.getItem("token");
 if (!token || token === "") {
     //console.log("Aucun token trouvé");
     displayEditMode(false);
+    switchLogBtn(false);
 } else {
     //console.log("Token trouvé : ", token);
     displayEditMode(true);
+    switchLogBtn(true);
 }
 
 /** Affiche ou masque les éléments de la page en fonction du mode d'édition
@@ -16,6 +18,24 @@ function displayEditMode(value) {
         element.style.display = value ? "block" : "none";
     });
 }
+
+/** Basculer l'affichage des boutons de connexion et de déconnexion
+ * @param {boolean} value - true pour afficher le bouton de déconnexion, false pour afficher le bouton de connexion
+ */
+function switchLogBtn (value) {
+    const loginBtn = document.querySelector(".login-btn");    
+    const logoutBtn = document.querySelector(".logout-btn");
+    
+    loginBtn.style.display = value ? "none" : "block";
+    logoutBtn.style.display = value ? "block" : "none";
+}
+
+/* Gère la déconnexion de l'utilisateur lorsque le bouton "logout" est cliqué */
+document.querySelector(".logout-btn").addEventListener("click", () => {
+    localStorage.removeItem("token"); // Supprime le token du localStorage
+    displayEditMode(false); // Masque les éléments d'édition
+    switchLogBtn(false); // Affiche le bouton de connexion et masque le bouton de déconnexion
+});
 
 /* Affiche la popup modale d'édition lorsque le bouton "Modifier" est cliqué */
 document.querySelector(".open-modal").addEventListener("click", () => {
